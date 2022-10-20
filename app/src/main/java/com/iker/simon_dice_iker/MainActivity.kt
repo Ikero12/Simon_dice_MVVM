0cd14b4c6b6e69f27079d683e0606c5873dc4a3c
Iker Rodriguez <irodrigueziglesias@danielcastelao.org>
2022-10-20 16:19:39 +0200

    Añadidas funciones para el jugador y colores de botones.

--- app/src/main/java/com/iker/simon_dice_iker/MainActivity.kt
+++ app/src/main/java/com/iker/simon_dice_iker/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.iker.simon_dice_iker
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.location.GnssAntennaInfo.Listener
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
@@ -16,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private var ronda = 1
     private var numbersArray= IntArray(1000)
     private var contador=0
+    private var puntuacion=0
     var numerointroducido:Int = 0
 
     lateinit var btnRojo:Button
@@ -23,6 +29,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnVerde:Button
     lateinit var btnAmarillo:Button
     lateinit var btnIniciar:Button
+    lateinit var score:TextView
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         btnAzul=findViewById(R.id.btn_azul)
         btnVerde=findViewById(R.id.btn_verde)
         btnAmarillo=findViewById(R.id.btn_amarillo)
+        score = findViewById(R.id.score)
         btnIniciar.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
                 inicio()
@@ -51,7 +61,7 @@ class MainActivity : AppCompatActivity() {
         for(i in 0..(ronda-1)){
             numbersArray[i]=0
         }
-
+        score.text = "Score: "
     }
 
     private fun colorRandom(){
@@ -63,6 +73,19 @@ class MainActivity : AppCompatActivity() {
 
         enterNumbers(numbersArray)
         Log.d("array:", Arrays.toString(numbersArray))
+
+
+
+        for (i in 0..numbersArray.size){
+            when (numbersArray[i]){
+                1-> {
+                    btnRojo.setBackgroundColor(R.color.red2)
+
+                }
+            }
+
+        }
+
 
     }
 
@@ -92,15 +115,19 @@ class MainActivity : AppCompatActivity() {
     private fun comprobante(){
         if(numerointroducido == numbersArray[contador] && contador != ronda){
             contador+=1
+
             if(contador==ronda){
                 comprobante()
             }
         }else if(contador == ronda || numerointroducido == numbersArray[contador]){
             ronda+=1
+            puntuacion+=10
+            score.text = "Score: " + puntuacion
             contador=0
             colorRandom()
         }else{
             Log.d("Perdiste wey","malo")
+            Toast.makeText(this, "Has hecho " + puntuacion + " puntos!",Toast.LENGTH_LONG).show()
             reset()
             btnIniciar.setVisibility(View.VISIBLE)
             ronda=1
