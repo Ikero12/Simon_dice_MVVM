6d51373971d3887268c1236e0910123b0a4f440d
Iker Rodriguez <irodrigueziglesias@danielcastelao.org>
2022-10-20 15:30:28 +0200

    Código totalmente funcional

--- app/src/main/java/com/iker/simon_dice_iker/MainActivity.kt
+++ app/src/main/java/com/iker/simon_dice_iker/MainActivity.kt
@@ -8,11 +8,12 @@ import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
+import kotlin.jvm.functions.FunctionN
 
 
 class MainActivity : AppCompatActivity() {
 
-    private var ronda = 0
+    private var ronda = 1
     private var numbersArray= IntArray(1000)
     private var contador=0
     var numerointroducido:Int = 0
@@ -41,22 +42,31 @@ class MainActivity : AppCompatActivity() {
         })
 
     }
+    private fun inicio(){
+        colorRandom()
+
+    }
+
+    private fun reset(){
+        for(i in 0..(ronda-1)){
+            numbersArray[i]=0
+        }
+
+    }
 
     private fun colorRandom(){
 
         for(i in 0..ronda){
             val randomValues = Random().nextInt(4) +1
-            numbersArray.set(ronda,randomValues)
+            numbersArray.set((ronda-1),randomValues)
         }
 
         enterNumbers(numbersArray)
         Log.d("array:", Arrays.toString(numbersArray))
+
     }
 
     private fun enterNumbers(numbersArray: IntArray){
-
-
-
         btnRojo.setOnClickListener(){
             Log.d("botónpresionado:","1")
             numerointroducido=1
@@ -77,28 +87,33 @@ class MainActivity : AppCompatActivity() {
             numerointroducido=4
             comprobante()
         }
-
-
     }
 
     private fun comprobante(){
         if(numerointroducido == numbersArray[contador] && contador != ronda){
             contador+=1
+            if(contador==ronda){
+                comprobante()
+            }
         }else if(contador == ronda || numerointroducido == numbersArray[contador]){
             ronda+=1
             contador=0
             colorRandom()
         }else{
             Log.d("Perdiste wey","malo")
+            reset()
             btnIniciar.setVisibility(View.VISIBLE)
-            ronda=0
+            ronda=1
             contador=0
         }
     }
 
-    private fun inicio(){
-        colorRandom()
-    }
+
+
+
+
+
+
 
 
 }
