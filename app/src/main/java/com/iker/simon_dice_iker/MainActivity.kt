6b93522b21550a7356121ae9818b6a31f344d20d
Iker <irodrigueziglesias@danielcastelao.org>
2022-11-07 09:53:33 +0100

    Añadida función record

--- app/src/main/java/com/iker/simon_dice_iker/MainActivity.kt
+++ app/src/main/java/com/iker/simon_dice_iker/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.iker.simon_dice_iker
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.TransitionDrawable
-import android.location.GnssAntennaInfo.Listener
+
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
 import java.util.*
-import kotlin.jvm.functions.FunctionN
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -32,7 +29,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnAmarillo:Button
     lateinit var btnIniciar:Button
     lateinit var score:TextView
-
+    lateinit var record:TextView
+    var puntuacion2: Int=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +41,8 @@ class MainActivity : AppCompatActivity() {
         btnVerde=findViewById(R.id.btn_verde)
         btnAmarillo=findViewById(R.id.btn_amarillo)
         score = findViewById(R.id.score)
+        record = findViewById(R.id.record)
+        loadRecord()
         btnIniciar.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
                 inicio()
@@ -81,45 +81,47 @@ class MainActivity : AppCompatActivity() {
         enterNumbers(numbersArray)
         Log.d("array:", Arrays.toString(numbersArray)) //Para comprobar la secuencia de colores de la máquina mediante la consola
 
-            val jobColores = GlobalScope.launch(Dispatchers.Main) {
 
-                for (i in 0..ronda){
-                    when (numbersArray[i]){
-                        1-> {
-                            btnRojo.setBackgroundColor(resources.getColor(R.color.red2))
-                            delay(750)
-                            btnRojo.setBackgroundColor(resources.getColor(R.color.red))
-                            delay(100)
-                        }
-                        2->{
-                            btnAzul.setBackgroundColor(resources.getColor(R.color.blue2))
-                            delay(750)
-                            btnAzul.setBackgroundColor(resources.getColor(R.color.blue))
-                            delay(100)
-                        }
-                        3->{
-                            btnVerde.setBackgroundColor(resources.getColor(R.color.green2))
-                            delay(750)
-                            btnVerde.setBackgroundColor(resources.getColor(R.color.green))
-                            delay(100)
-                        }
-                        4->{
-                            btnAmarillo.setBackgroundColor(resources.getColor(R.color.yellow2))
-                            delay(750)
-                            btnAmarillo.setBackgroundColor(resources.getColor(R.color.yellow))
-                            delay(100)
-                        }
-                    }
-
-                }
-                booleanBotones=true
-                activarBotones(booleanBotones)
-            }
-
-        jobColores
-
+        jobColores()
     }
 
+
+    private fun jobColores(){
+        val jobColores = GlobalScope.launch(Dispatchers.Main) {
+
+            for (i in 0..ronda){
+                when (numbersArray[i]){
+                    1-> {
+                        btnRojo.setBackgroundColor(resources.getColor(R.color.red2))
+                        delay(750)
+                        btnRojo.setBackgroundColor(resources.getColor(R.color.red))
+                        delay(100)
+                    }
+                    2->{
+                        btnAzul.setBackgroundColor(resources.getColor(R.color.blue2))
+                        delay(750)
+                        btnAzul.setBackgroundColor(resources.getColor(R.color.blue))
+                        delay(100)
+                    }
+                    3->{
+                        btnVerde.setBackgroundColor(resources.getColor(R.color.green2))
+                        delay(750)
+                        btnVerde.setBackgroundColor(resources.getColor(R.color.green))
+                        delay(100)
+                    }
+                    4->{
+                        btnAmarillo.setBackgroundColor(resources.getColor(R.color.yellow2))
+                        delay(750)
+                        btnAmarillo.setBackgroundColor(resources.getColor(R.color.yellow))
+                        delay(100)
+                    }
+                }
+            }
+            booleanBotones=true
+            activarBotones(booleanBotones)
+        }
+        jobColores
+    }
 
 
     private fun enterNumbers(numbersArray: IntArray){
@@ -178,17 +180,16 @@ class MainActivity : AppCompatActivity() {
             contador=0
             colorRandom()
         }else{
-            Log.d("Perdiste wey","malo")
             Toast.makeText(this, "Has hecho " + puntuacion + " puntos!",Toast.LENGTH_LONG).show()
             reset()
+            saveRecord()
+            loadRecord()
             btnIniciar.setVisibility(View.VISIBLE)
             puntuacion=0
             ronda=1
             contador=0
         }
     }
-
-
 
     fun activarBotones(boolean: Boolean){
         btnRojo.isClickable == boolean
@@ -198,8 +199,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+ private fun saveRecord(){
 
+     if(puntuacion>puntuacion2){
+         puntuacion2 = puntuacion
+         val sharedPreferences = getSharedPreferences("record", Context.MODE_PRIVATE)
+         val editor = sharedPreferences.edit()
+         editor.apply(){
+             putInt("INT_KEY",puntuacion2)
+         }.apply()
+     }
+     Toast.makeText(this,"Se ha establecido un nuevo record!",Toast.LENGTH_LONG)
+ }
 
+    private fun loadRecord(){
+        val sharedPreferences = getSharedPreferences("record", Context.MODE_PRIVATE)
+        val savedRecord = sharedPreferences.getInt("INT_KEY",0)
+        record.text = "Record: " + savedRecord
+    }
 
 
 
